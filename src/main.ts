462c165917721d43f44a5bb5f16651b1ca9b1cfc
Dawid Liszka <dawid.liszka@netguru.pl>
2020-04-05 15:27:04 +0200

    Add OpenAPI documentation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common'
+
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe())
+
+  const options = new DocumentBuilder()
+    .setTitle('ISStifier documentation')
+    .setDescription('The ISStifier API description')
+    .setVersion('1.0')
+    .addTag('ISStifier')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api', app, document)
 
   await app.listen(process.env.PORT || 3000);
 }
