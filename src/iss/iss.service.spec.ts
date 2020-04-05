8c90390f7fd895b1874773bf661c85c27d9d11d2
Dawid Liszka <dawid.liszka@netguru.pl>
2020-04-05 16:09:54 +0200

    Add example test

--- src/iss/iss.service.spec.ts
+++ src/iss/iss.service.spec.ts
@@ -13,6 +13,21 @@ describe('IssService', () => {
   });
 
   it('should be defined', () => {
+    // Arrange
+    // Act
+    // Assert
     expect(service).toBeDefined();
   });
+
+  it('should return proper response', async () => {
+    // Arrange
+    const token = 'some_nice_toke_here'
+    const message: any = { body: 'Body', title: 'Title' }
+
+    // Act
+    const response = await service.createSubscription(token, message)
+
+    // Arrage
+    expect(response).toBeDefined()
+  })
 });
